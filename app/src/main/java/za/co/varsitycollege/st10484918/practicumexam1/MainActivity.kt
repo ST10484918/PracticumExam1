567c0b98e013bb6033b6da3728f8fe9c24fedc37
ST10484918 <ST10484918@vcconnect.edu.za>
2025-06-19 10:50:23 +0200

    hii

--- app/src/main/java/za/co/varsitycollege/st10484918/practicumexam1/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10484918/practicumexam1/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,51 +31,91 @@ class MainActivity : AppCompatActivity() {
     val Button3 = findViewById<Button>(R.id.button3)
     val EditText = findViewById<EditText>(R.id.EditText)
     val TextView2 = findViewById<TextView>(R.id.TextView2)
-    val Button4 = findViewById<Button>(R.id.button4)
+    val Button4
+        get() = findViewById<Button>(R.id.button4)
 
-    //setonclicklistener
-    Button1.setOnClickListener
-    {
-        Toast.makeText(this, "Add Playlist", Toast.LENGTH_SHORT).show()
-        val nameofplaylist = EditText.text.toString().trim()
+            //setOnClicklistener
 
-        //when statements//
-        val string = when (nameofplaylist) {
-            "The song title" -> "Millions"
-            "The artist's name" -> "Young Thug"
-            "The rating(1-5)" -> 4
-            "User comments" -> "Rap"
-            else -> "Please insert the given information"
-        }
-        val string = when (nameofplaylist) {
-            "The song title" -> "Bad Blood"
-            "The artist's name" -> "Taylor Swift"
-            "The rating" -> 1
-            "User comments" -> "Dance Song"
-            else -> "Please insert the given information"
-        }
-        val string = when (nameofplaylist) {
-            "The song title" -> "Clouded"
-            "The artist's name" -> "Brent Faiyaz"
-            "The rating" -> 2
-            "User comments" -> "Best love song"
-            else -> "Please insert the given information"
-        }
-        val string = when (nameofplaylist) {
-            "The song title" -> "Memories"
-            "The artist's name" -> "Maroon 5"
-            "The rating" -> 3
-            "User comments" -> "Song of memories that were lost"
-            else -> "please enter the given information"
-        }
-        Button2.setOnClickListener {
-            Toast.makeText(this, "Navigate to next screen", Toast.LENGTH_SHORT).show()
-            val nameofplaylist = EditText.text.clear()
-        }
-        Button3.setOnClickListener {
-            Toast.makeText(this, "Exit the app", Toast.LENGTH_SHORT).show()
-            val nameofplaylist = EditText.text.clear()
-        }
+
+            .layout
+            .text
+            .run() {
+                Toast.makeText(this, "Add Playlist", Toast.LENGTH_SHORT).show()
+                val nameofplaylist = EditText.text.toString().trim()
+
+
+                //when statements//
+                val settring = when (nameofplaylist) {
+                    "The song title" -> {
+                        "Millions"
+                    }
+
+                    "The artist's name" -> {
+                        "Young Thug"
+                    }
+
+                    "The rating(1-5)" -> {
+                        4
+                    }
+
+                    "User comments" -> {
+                        "Rap"
+                    }
+
+                    else -> {
+                        "Please insert the given information"
+                    }
+                }
+                val Settring = when (nameofplaylist) {
+                    "The song title" -> {
+                        "Bad Blood"
+                    }
+
+                    "The artist's name" -> {
+                        "Taylor Swift"
+                    }
+
+                    "The rating" -> {
+                        1
+                    }
+
+                    "User comments" -> {
+                        "Dance Song"
+                    }
+
+                    else -> {
+                        "Please insert the given information"
+                    }
+                }
+                val settring = when (nameofplaylist) {
+                    "The song title" -> "Clouded"
+                    "The artist's name" -> "Brent Faiyaz"
+                    "The rating" -> 2
+                    "User comments" -> "Best love song"
+                    else -> "Please insert the given information"
+                }
+                val settring = when (nameofplaylist) {
+                    "The song title" -> "Memories"
+                    "The artist's name" -> "Maroon 5"
+                    "The rating" -> 3
+                    "User comments" -> "Song of memories that were lost"
+                    else -> "please enter the given information"
+                }
+                Button2.setOnClickListener {
+                    Toast.makeText(this, "Navigate to next screen", Toast.LENGTH_SHORT).show()
+                    val nameofplaylist = EditText.text.clear()
+                }
+                Button3.setOnClickListener {
+                    Toast.makeText(this, "Exit the app", Toast.LENGTH_SHORT).show()
+                    val nameofplaylist = EditText.text.clear()
+                }
+
+                //Reset button setonclicker
+                Button3.setOnClickListener {
+                    //    var feedback = Answer1.text.toString().trim()
+                    EditText.text.clear()
+                    TextView2.text = "Reset App"
+                }
 
 
 
